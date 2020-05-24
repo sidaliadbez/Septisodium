e0c3c19b90324affa1c24dfce3a5b9fb8302f739
sidaliadbez <hs_zebda@esi.dz>
2020-05-24 20:54:53 +0100

    Initial commit - menu V1

---
+++ app/src/main/java/com/example/septisodium/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.septisodium;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation) ;
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        BottomNavigationView.OnNavigationItemSelectedListener navListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        switch (item.getItemId()) {
+                            case R.id.page_1:
+
+                                break;
+                            case R.id.page_2:
+
+                                break;
+
+                        }
+
+                        return true;
+                    }
+                };
+    }
+}

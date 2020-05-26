1b4915f2b509da4c66d6de5a43826dfdf8aad8a5
sidaliadbez <hs_zebda@esi.dz>
2020-05-27 00:07:52 +0100

    expandble item V1

--- app/src/main/java/com/example/septisodium/StepbyStepActivity.java
+++ app/src/main/java/com/example/septisodium/StepbyStepActivity.java
@@ -3,17 +3,23 @@ package com.example.septisodium;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class StepbyStepActivity extends AppCompatActivity {
+    Boolean exp =false ;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +28,25 @@ public class StepbyStepActivity extends AppCompatActivity {
        Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(3);
         menuItem.setChecked(true);
+        LinearLayout linearLayout ;
+        final RelativeLayout expandableLayout ;
+        linearLayout= findViewById(R.id.linear_layout);
+        expandableLayout= findViewById(R.id.expandable_layout);
+        expandableLayout.setVisibility(View.GONE);
 
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!exp){
+                    exp=true ;
+                    expandableLayout.setVisibility(View.VISIBLE);
+                }else {
+                    exp=false ;
+                    expandableLayout.setVisibility(View.GONE);
+                }
+            }
+        });
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {

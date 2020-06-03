c4f255565781c66d44409fbc545a1fe95f289d64
sidaliadbez <hs_zebda@esi.dz>
2020-06-03 21:12:45 +0100

    FAQ v1.5

--- app/src/main/java/com/example/septisodium/FaqActivity.java
+++ app/src/main/java/com/example/septisodium/FaqActivity.java
@@ -7,18 +7,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class FaqActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class FaqActivity extends AppCompatActivity {
+private ArrayList<String> header = new ArrayList<>();
+    private ArrayList<ArrayList<String>> body = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        ArrayList<String> contenu1 = new ArrayList<>();
+        contenu1.add("quelque chose");
+        ArrayList<String> contenu2 = new ArrayList<>();
+        contenu2.add("quelque chose2");
+header.add("Question1");
+header.add("Question2");
+body.add(contenu1);
+body.add(contenu2);
+        ExpandableListView expandableListView = findViewById(R.id.expandableListView);
+        expandableListView.setAdapter(new ExpendableListAdapter(this,expandableListView,header,body));
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation) ;
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(0);

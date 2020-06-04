6c83696faaef7abfc02a659609f6df8b1aee69e7
sidaliadbez <hs_zebda@esi.dz>
2020-06-04 23:47:38 +0100

    cards added to all fragments

--- app/src/main/java/com/example/septisodium/TravelFragment.java
+++ app/src/main/java/com/example/septisodium/TravelFragment.java
@@ -3,10 +3,15 @@ package com.example.septisodium;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,7 +28,10 @@ public class TravelFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager ;
+    View v ;
+    private List<objectTemoin> objectTemoinList ;
     public TravelFragment() {
         // Required empty public constructor
     }
@@ -34,11 +42,11 @@ public class TravelFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TravelFragment.
+     * @return A new instance of fragment ExploreFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TravelFragment newInstance(String param1, String param2) {
-        TravelFragment fragment = new TravelFragment();
+    public static ExploreFragment newInstance(String param1, String param2) {
+        ExploreFragment fragment = new ExploreFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,12 +61,30 @@ public class TravelFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        objectTemoinList= new ArrayList<>();
+        objectTemoinList.add(new objectTemoin("un nom","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom3","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom4","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom5","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom3","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom4","truc1","truc2","truc3"));
+        objectTemoinList.add(new objectTemoin("un nom5","truc1","truc2","truc3"));
+
+        objectTemoinList.add(new objectTemoin("un nom2","truc1","truc2","truc3"));
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        v=inflater.inflate(R.layout.fragment_explore, container, false);
+        recyclerView= (RecyclerView)  v.findViewById(R.id.recyclerview1);
+        RecyclerviewAdapter recyclerviewAdapter = new RecyclerviewAdapter(getContext(),objectTemoinList);
+        layoutManager = new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerviewAdapter);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_travel, container, false);
+        return v;
     }
 }
